dd6581c95606f552fc909c256f8bb7283ee29603
GMCamargo <bielmarcom@gmail.com>
2020-07-20 20:28:07 -0300

    Setup Server Express

---
+++ bin/server.js
@@ -0,0 +1,12 @@
+const app = require('../src/app')
+const http = require('http');
+
+const port  = (process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log('Api rodando na porta: ' + port)
+
--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
-const port = 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
 const router = express.Router();
 
 const route = router.get('/', (req,res,next) =>{
@@ -15,7 +10,7 @@ const route = router.get('/', (req,res,next) =>{
     });
 });
 
-app.use('/', route);
-server.listen(port)
-console.log('Api rodando na porta: ' + port)
 
+app.use('/', route);
+
+module.exports = app;
